e7e2a27f75070cf10acd06444703fc244b09a380
Makeev-AC19-04 <70644348+Makeev-AC19-04@users.noreply.github.com>
2020-09-02 16:59:01 +0300

    Add files via upload

---
+++ Lab0Makeev/Lab0Makeev/Lab0Makeev.cpp
@@ -0,0 +1,16 @@
+// Lab0Makeev.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	int ch;
+	std::cout << "Hello world!" << std::endl;
+	std::cin >> ch;
+	std::cout << 10 / ch;
+	return 0;
+}
+
